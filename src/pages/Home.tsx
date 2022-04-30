6c489773b22bd66e26ca8766baaaaf25771974d2
Mac_Developer <araozangel842@gmail.com>
2022-04-30 19:43:09 +0300

    Fetching by Name

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,30 +1,52 @@
 import { useState } from 'react';
+
 import Button from '../components/Button';
 import { Input } from '../components/Input';
-import withFetchHook from '../fetchHook/FetchHook';
+import withFetch from '../HOC/withFetch';
+import Country from './Country';
 import { AllData } from '../interfaces/interfaces.js';
+import '../sass/App.scss';
 
-interface Props {
-  data: AllData[];
+interface HomeProps {
+  data?: AllData[];
+  handleSearch?: (value: string) => void;
+  searchedCountries?: AllData[];
 }
 
-const Home = ({ data }: Props) => {
+const Home = ({ data, handleSearch, searchedCountries }: HomeProps) => {
+  // console.log(data);
+  console.log('searchedCountries', searchedCountries);
   const [value, setValue] = useState('');
-  const handleValue = (e: any) => setValue(e.target.value);
+  const handleValue = (e: any) => {
+    if (handleSearch) {
+      handleSearch(e.target.value);
+    }
+    setValue(e.target.value);
+  };
   console.log(value);
-  console.log(data);
+  // console.log(data);
   const handleClick = () => {
     console.log('clicked');
   };
+
+  const handleRenderCountries = (countries: AllData[] | undefined) => {
+    return countries?.map((c, index: number) => {
+      return <Country key={index} country={c} />;
+    });
+  };
+
   return (
-    <div className="">
+    <div className="home">
       Home
       <Input value={value} onChange={handleValue} />
-      <Button label="Click me" size="sm" onClick={handleClick} className={''} />
-      <Button label="Click me" size="md" onClick={handleClick} className={''} />
-      <Button label="Click me" size="lg" onClick={handleClick} className={''} />
+      <div className="buttonSection">
+        <Button label="Click me" size="sm" onClick={handleClick} className={''} />
+        <Button label="Click me" size="md" onClick={handleClick} className={''} />
+        <Button label="Click me" size="lg" onClick={handleClick} className={''} />
+      </div>
+      {searchedCountries ? handleRenderCountries(searchedCountries) : handleRenderCountries(data)}
     </div>
   );
 };
 
-export default withFetchHook(Home, 'https://restcountries.com/v3.1/all');
+export default withFetch(Home, 'https://restcountries.com/v3.1/all');
